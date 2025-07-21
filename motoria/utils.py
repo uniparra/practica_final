af9f7043a81a6081c2d0654d40c138d433e34221
Gaizka Menéndez Hernández <gaizkam20@gmail.com>
2025-07-21 23:28:22 +0200

    More util functions

--- motoria/utils.py
+++ motoria/utils.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import precision_recall_curve, auc
 import os
 
 
-def plot_confusion_v2(model, X_test, y_test, name_model, output_dir="results/plots"):
+def plot_confusionMatrix_v2(model, X_test, y_test, name_model, output_dir="results/plots"):
     
     filename= f"confusion_matrix_{name_model}.png"
 
@@ -18,7 +19,7 @@ def plot_confusion_v2(model, X_test, y_test, name_model, output_dir="results/plo
     print(f"Matriz de confusión guardada en: {filepath}")
 
     
-def training_accuracy(history, name_model, output_dir="results/plots"):
+def plot_training_accuracy(history, name_model, output_dir="results/plots"):
     filename= f"training_acc_{name_model}.png"
     fig, ax = plt.subplots(figsize=(8, 8))
     ax.set_title("Precisión durante el entrenamiento")
@@ -36,7 +37,7 @@ def training_accuracy(history, name_model, output_dir="results/plots"):
     print(f"Curva de precisión durante entrenamiento guardada en: {filepath}")
     
 
-def training_loss(history, name_model, output_dir="results/plots"):
+def plot_training_loss(history, name_model, output_dir="results/plots"):
     filename= f"training_loss_{name_model}.png"
     fig, ax = plt.subplots(figsize=(8, 8))
     ax.set_title("Pérdidas durante el entrenamiento")
@@ -52,3 +53,22 @@ def training_loss(history, name_model, output_dir="results/plots"):
     fig.savefig(filepath)
     plt.close(fig)
     print(f"Curva de pérdidas durante el entrenamiento guardada en: {filepath}")
+    
+
+def plot_precision_recall_curve(name_model, y_test, y_pred_proba, output_dir="results/plots"):
+    
+    filename= f"precision_recall_{name_model}.png"
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.set_title("Curva Precision-Recall")
+    precision, recall, _ = precision_recall_curve(y_test, y_pred_proba)
+    auc_pr = auc(recall, precision)
+    ax.plot(recall, precision, marker='.', label=f'AUC-PR = {auc_pr:.2f}')
+    ax.set_xlabel('Precisión')
+    ax.set_ylabel('Recall')
+    ax.legend(loc="lower left")
+    
+    os.makedirs(output_dir, exist_ok=True)
+    filepath = os.path.join(output_dir, filename)
+    fig.savefig(filepath)
+    plt.close(fig)
+    print(f"Curva Precision-Recall guardada en: {filepath}")
